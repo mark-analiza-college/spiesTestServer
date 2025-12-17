967dcce5946485412ddfa3a537248c4d0f534e19
mark-analiza-college <mark@analiza-college.co.il>
2025-12-17 10:58:18 +0200

    added root

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("<div style='display: flex; justify-content: center; align-items: center; height: 100vh;'><h1>Welcome to the Spies Test Server</h1></br><p>This is a simple server for the Spies Test. Behave yourself and Happy Chanuka!</p></div>")
+app.get("/", (req, res) => {
+  res.send(
+    `<div style='display: flex; justify-content: center;  height: 100vh;'><div><h1>Welcome to the Spies Test Server</h1><p>This is a simple server for the Spies Test. Behave yourself and Happy Chanuka!</p></div></div>`
+  );
 });
 
-app.get('/people', (req, res) => {
-    const filePath = path.join(__dirname, 'data', 'people.js');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) return res.status(500).send('Could not read people file');
-        res.type('application/javascript').send(data);
-    });
+app.get("/people", (req, res) => {
+  const filePath = path.join(__dirname, "data", "people.js");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Could not read people file");
+    res.type("application/javascript").send(data);
+  });
 });
 
-app.get('/transcriptions', (req, res) => {
-    const filePath = path.join(__dirname, 'data', 'transcriptions.js');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) return res.status(500).send('Could not read transcriptions file');
-        res.type('application/javascript').send(data);
-    });
+app.get("/transcriptions", (req, res) => {
+  const filePath = path.join(__dirname, "data", "transcriptions.js");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Could not read transcriptions file");
+    res.type("application/javascript").send(data);
+  });
 });
 
-app.get('/report', (req, res) => {
-    res.send('thank you agent!');
+app.get("/report", (req, res) => {
+  res.send("thank you agent!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
